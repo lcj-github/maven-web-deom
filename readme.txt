e65e267aa57382cd611b7da38eb1e15bb5baa865
李 春建 <lichunjian@gw.com.cn>
2017-03-21 16:36:17 +0800

    mockito demo ，模拟dao层，验证service层

--- readme.txt
+++ readme.txt
@@ -1,2 +1,5 @@
 1：TestHello TestSquare 与  jenkins自动化回归测试
-2：TestMockito  mock测试
+2：TestMockito  mock测试基本语法
+3：mockitodemo 实例  http://blog.csdn.net/zhoudaxia/article/details/33056093
+1) 在进行模拟测试时，要先设置模拟对象上方法的返回预期值，执行测试时会调用模拟对象上的方法，因此要验证这些方法是否被调用，并且传入的参数值是否符合预期。
+PersonServiceTest  验证了 更新及不更新Person情况，
---
+++ src/test/java/com/lcj/mockitodemo/Person.java
@@ -0,0 +1,23 @@
+package com.lcj.mockitodemo;
+
+public class Person {
+	
+	private final Integer personID;
+    private final String personName;
+    public Person( Integer personID, String personName )
+    {
+        this.personID = personID;
+        this.personName = personName;
+    }
+    public Integer getPersonID()
+    {
+        return personID;
+    }
+    public String getPersonName()
+    {
+        return personName;
+    }
+
+}
+
+ 
