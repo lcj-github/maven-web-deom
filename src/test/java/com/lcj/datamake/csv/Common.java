832f0d3ed5e44133e0a959c63845e1966a2f0547
李 春建 <lichunjian@gw.com.cn>
2017-04-14 17:13:25 +0800

    csv 读取

--- src/test/java/com/lcj/datamake/csv/Common.java
+++ src/test/java/com/lcj/datamake/csv/Common.java
@@ -11,6 +11,8 @@ public class Common {
 	public static final String PROCESSING = "Processing...";
     public static final String  phoneCall_INFO_XLSX_PATH = LIB_PATH + "/phonecall" + POINT + OFFICE_EXCEL_2010_POSTFIX;
     public static final String NOT_EXCEL_FILE = " : Not the Excel file!";
+    public static final String STUDENT_INFO_XLS_PATH = LIB_PATH + "/student_info" + POINT + OFFICE_EXCEL_2003_POSTFIX;
+    public static final String STUDENT_INFO_XLSX_PATH = LIB_PATH + "/student_info" + POINT + OFFICE_EXCEL_2010_POSTFIX;
     
   
 }
